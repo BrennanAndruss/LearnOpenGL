095dd8a9b970c9e14e38931b3a6e378845df19d4
brennanandruss@gmail.com <brennanandruss@gmail.com>
2024-12-31 09:38:48 -0800

    Refactored and expanded shader class

--- src/shader.h
+++ src/shader.h
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <unordered_map>
 
 class Shader
 {
@@ -55,42 +56,25 @@ public:
 
 		// Compile shaders
 		unsigned int vertex, fragment;
-		int compiled;
-		char infoLog[512];
 
 		// Create and configure the vertex shader
 		vertex = glCreateShader(GL_VERTEX_SHADER);
 		glShaderSource(vertex, 1, &vShaderCode, NULL);
 		glCompileShader(vertex);
-		glGetShaderiv(vertex, GL_COMPILE_STATUS, &compiled);
-		if (!compiled)
-		{
-			glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-			std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
+		checkCompileErrors(vertex, ShaderType::VERTEX);
 
 		// Create and configure the fragment shader
 		fragment = glCreateShader(GL_FRAGMENT_SHADER);
 		glShaderSource(fragment, 1, &fShaderCode, NULL);
 		glCompileShader(fragment);
-		glGetShaderiv(fragment, GL_COMPILE_STATUS, &compiled);
-		if (!compiled)
-		{
-			glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-			std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
+		checkCompileErrors(fragment, ShaderType::FRAGMENT);
 
 		// Attach and link compiled shaders to the shader program object
 		ID = glCreateProgram();
 		glAttachShader(ID, vertex);
 		glAttachShader(ID, fragment);
 		glLinkProgram(ID);
-		glGetProgramiv(ID, GL_LINK_STATUS, &compiled);
-		if (!compiled)
-		{
-			glGetProgramInfoLog(ID, 512, NULL, infoLog);
-			std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-		}
+		checkLinkingErrors(ID);
 
 		// Delete shaders once linked
 		glDeleteShader(vertex);
@@ -115,5 +99,101 @@ public:
 	void setFloat(const std::string& name, float value) const
 	{
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+	}
+
+	void setVec2(const std::string& name, const glm::vec2& value) const
+	{
+		glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+
+	void setVec2(const std::string& name, float x, float y) const
+	{
+		glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+	}
+
+	void setVec3(const std::string& name, const glm::vec3& value) const
+	{
+		glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+
+	void setVec3(const std::string& name, float x, float y, float z) const
+	{
+		glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+	}
+
+	void setVec4(const std::string& name, const glm::vec4& value) const
+	{
+		glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+	}
+
+	void setVec4(const std::string& name, float x, float y, float z, float w) const
+	{
+		glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+	}
+
+	void setMat2(const std::string& name, const glm::mat2& mat) const
+	{
+		glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
+	void setMat3(const std::string& name, const glm::mat3& mat) const
+	{
+		glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
+	void setMat4(const std::string& name, const glm::mat4& mat) const
+	{
+		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+	}
+
+private:
+	enum class ShaderType
+	{
+		VERTEX,
+		FRAGMENT,
+		// other shader types...
+	};
+
+	// Utility function for checking shader compilation errors
+	void checkCompileErrors(GLuint shader, ShaderType type)
+	{
+		GLint success;
+		GLchar infoLog[512];
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(shader, 512, NULL, infoLog);
+			std::cerr << "ERROR::SHADER::"
+				<< shaderTypeToString(type)
+				<< "::COMPILATION_FAILED\n" 
+				<< infoLog << std::endl;
+		}
+	}
+
+	// Utility function for checking shader linking errors
+	void checkLinkingErrors(GLuint program)
+	{
+		GLint linked;
+		GLchar infoLog[512];
+		glGetProgramiv(program, GL_LINK_STATUS, &linked);
+		if (!linked)
+		{
+			glGetProgramInfoLog(program, 512, NULL, infoLog);
+			std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+				<< infoLog << std::endl;
+		}
+	}
+
+	// Helper function to convert ShaderType to a string
+	std::string shaderTypeToString(ShaderType type)
+	{
+		// Initialize a static lookup table on first function call
+		static const std::unordered_map<ShaderType, std::string> enumToString{
+			{ ShaderType::VERTEX, "VERTEX" },
+			{ ShaderType::FRAGMENT, "FRAGMENT" },
+			// other shader types...
+		};
+
+		return enumToString.at(type);
 	}
 };
